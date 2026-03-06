c8ec4ff4098f332f947d03b23149ccf51e9f0abd
Gyanprakash9771 <gyanprakash9771@gmail.com>
2026-03-06 16:59:54 +0530

    fix canvas

--- src/components/fabric-components/hero.jsx
+++ src/components/fabric-components/hero.jsx
@@ -3894,8 +3894,8 @@ function Hero() {
   };
   const designs = [
     "/designs/images(6).jpg",
-    "/designs/images(6).jpg",
-    "/designs/images(6).jpg"
+    "/designs/images(7).jpg",
+    "/designs/images(8).jpg"
   ];
 
   const addDesignToCanvas = (url) => {
