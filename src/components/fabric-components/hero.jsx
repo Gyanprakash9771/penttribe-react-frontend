873a30413bf56636246afa4a3a6f3039b77ecf9a
Gyanprakash9771 <gyanprakash9771@gmail.com>
2026-03-09 11:41:11 +0530

    canvas fix

--- src/components/fabric-components/hero.jsx
+++ src/components/fabric-components/hero.jsx
@@ -8370,7 +8370,7 @@ const fontChange = (event) => {
                     </div>
                     
                   </div>
-                </div>
+                
                 {addText ? (
                   <div
                     className="row"
@@ -8386,27 +8386,25 @@ const fontChange = (event) => {
                         value={fontValue}
                       >
                        <option value="select">Select a Font</option>
-
-<option value="Arial" style={{fontFamily:"Arial"}}>Arial</option>
-<option value="Times New Roman" style={{fontFamily:"Times New Roman"}}>Times New Roman</option>
-<option value="Verdana" style={{fontFamily:"Verdana"}}>Verdana</option>
-<option value="Courier New" style={{fontFamily:"Courier New"}}>Courier New</option>
-
-<option value="Poppins" style={{fontFamily:"Poppins"}}>Poppins</option>
-<option value="Roboto" style={{fontFamily:"Roboto"}}>Roboto</option>
-<option value="Montserrat" style={{fontFamily:"Montserrat"}}>Montserrat</option>
-<option value="Lobster" style={{fontFamily:"Lobster"}}>Lobster</option>
-<option value="Oswald" style={{fontFamily:"Oswald"}}>Oswald</option>
-<option value="Raleway" style={{fontFamily:"Raleway"}}>Raleway</option>
-<option value="Playfair Display" style={{fontFamily:"Playfair Display"}}>Playfair Display</option>
-<option value="Ubuntu" style={{fontFamily:"Ubuntu"}}>Ubuntu</option>
-<option value="Pacifico" style={{fontFamily:"Pacifico"}}>Pacifico</option>
-<option value="Anton" style={{fontFamily:"Anton"}}>Anton</option>
-<option value="Bebas Neue" style={{fontFamily:"Bebas Neue"}}>Bebas Neue</option>
-<option value="Nunito" style={{fontFamily:"Nunito"}}>Nunito</option>
-<option value="Merriweather" style={{fontFamily:"Merriweather"}}>Merriweather</option>
-<option value="Dancing Script" style={{fontFamily:"Dancing Script"}}>Dancing Script</option>
-<option value="Quicksand" style={{fontFamily:"Quicksand"}}>Quicksand</option>
+                      <option value="Arial" style={{fontFamily:"Arial"}}>Arial</option>
+                      <option value="Times New Roman" style={{fontFamily:"Times New Roman"}}>Times New Roman</option>
+                      <option value="Verdana" style={{fontFamily:"Verdana"}}>Verdana</option>
+                      <option value="Courier New" style={{fontFamily:"Courier New"}}>Courier New</option>
+                      <option value="Poppins" style={{fontFamily:"Poppins"}}>Poppins</option>
+                      <option value="Roboto" style={{fontFamily:"Roboto"}}>Roboto</option>
+                      <option value="Montserrat" style={{fontFamily:"Montserrat"}}>Montserrat</option>
+                      <option value="Lobster" style={{fontFamily:"Lobster"}}>Lobster</option>
+                      <option value="Oswald" style={{fontFamily:"Oswald"}}>Oswald</option>
+                      <option value="Raleway" style={{fontFamily:"Raleway"}}>Raleway</option>
+                      <option value="Playfair Display" style={{fontFamily:"Playfair Display"}}>Playfair Display</option>
+                      <option value="Ubuntu" style={{fontFamily:"Ubuntu"}}>Ubuntu</option>
+                      <option value="Pacifico" style={{fontFamily:"Pacifico"}}>Pacifico</option>
+                      <option value="Anton" style={{fontFamily:"Anton"}}>Anton</option>
+                      <option value="Bebas Neue" style={{fontFamily:"Bebas Neue"}}>Bebas Neue</option>
+                      <option value="Nunito" style={{fontFamily:"Nunito"}}>Nunito</option>
+                      <option value="Merriweather" style={{fontFamily:"Merriweather"}}>Merriweather</option>
+                      <option value="Dancing Script" style={{fontFamily:"Dancing Script"}}>Dancing Script</option>
+                      <option value="Quicksand" style={{fontFamily:"Quicksand"}}>Quicksand</option>
                       </select>
                     </div>
                     {/* <div className="col-md-6 mt-2">
@@ -8455,6 +8453,7 @@ const fontChange = (event) => {
                 ) : (
                   <></>
                 )}
+                </div>
                 {/* <div className="row">
                   <div className="col-md-12">
                     <input
